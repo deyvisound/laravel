ad85a455fc6f6c245416a6978f8e3d4240222a31
Deyvison Estevam <deyvisonestevam@gmail.com>
2019-12-15 20:18:05 -0300

    Iniciando projeto Laravel

---
+++ cursoLaravel/app/Http/Controllers/ContatoController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContatoController extends Controller
+{
+    public function index(){
+		return "Este é o index do Controller contatoController";     	
+    }
+
+    public function criar(){
+
+    }
+
+    public function editar(){
+    	
+    }
+}
